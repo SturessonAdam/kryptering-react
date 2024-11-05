8d9f10f1229224782af0f393c2546bb773ccacb9
Adam <Adam.sturesson@live.se>
2024-11-05 14:18:13 +0100

    cypress-test encrypt

--- src/pages/Home.cy.jsx
+++ src/pages/Home.cy.jsx
@@ -1,14 +1,26 @@
 import Home from './Home'
 
-
 describe('<Home />', () => {
 
   beforeEach(() => {
     cy.mount(<Home />)
-  })
+  });
 
   it('renders', () => {
     cy.mount(<Home />)
+  });
+
+  it('renders the form', () => {
+    cy.get('form').should('exist')
+  });
+
+  it('should be able to encrypt a text and display the result', () => {
+    cy.get('input[placeholder="word to encrypt/decrypt"]').type('hello world')
+    cy.get('input[placeholder="shift number"]').type('3')
+    cy.get('select').select('Kryptera')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('p').contains('khoor zruog').should('exist')
   })
 
 })
